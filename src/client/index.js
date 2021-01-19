098726a348a4b8e3c226830b9fadf4d30bc5f50b
Emiri Begic <emiri.begic@gmail.com>
2021-01-19 17:22:04 +0100

    Update the design on hero sectionn

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,14 @@
 import { example } from './js/example';
 import './styles/main.scss';
 
+const navbar = document.querySelector('.page__header');
+const hero = document.querySelector('.contents__hero');
+
+// Dynamically get the height of the navbar and calculate the hight of hero
+window.addEventListener('load', () => {
+	const navbarHeight = navbar.offsetHeight;
+	console.log(navbarHeight);
+	hero.style.height = `calc(100vh - ${navbarHeight}px)`;
+});
+
 export { example };
