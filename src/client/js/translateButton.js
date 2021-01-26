e8fa4803bdaf058d681ee635351dd6ea84d7e8eb
Emiri Begic <emiri.begic@gmail.com>
2021-01-26 20:31:23 +0100

    Add buttons in about section

--- src/client/js/translateButton.js
+++ src/client/js/translateButton.js
@@ -5,24 +5,20 @@ const btnJap = document.querySelector('.btn-jap');
 
 const translateToGerman = btnGer.addEventListener('click', function () {
 	aboutText.innerHTML = `<p>
-	<h3>Ich bin Emiri</h3>, a self-taught front-end developer in
-	Germany.<br>
-	I like to organize things and wondered if I could make my own To-Do app to make
-	my
-	life
-	easier.
-	Since then coding (especially JavaScript!) has triggered my curiosity and it was enough
-	to
-	motivate me to change my career into the tech industry.<br>
-	My goal as front-end developer is to make modern
-	web
-	applications that make our lives a bit easier!
-	</p>
-	<div class="about-translation">
-		<button class="btn btn-eng">Englisch</button>
-		<button class="btn btn-ger">Deutsch</button>
-		<button class="btn btn-jap">Japanisch</button>
-	</div>`;
+    <h3>Ich bin Emiri</h3>, a self-taught front-end developer in
+    Germany.<br>
+    I like to organize things and wondered if I could make my own To-Do app to make
+    my
+    life
+    easier.
+    Since then coding (especially JavaScript!) has triggered my curiosity and it was enough
+    to
+    motivate me to change my career into the tech industry.<br>
+    My goal as front-end developer is to make modern
+    web
+    applications that make our lives a bit easier!
+    </p>
+    <p>Translate in:</p>`;
 });
 
 export { translateToGerman };
