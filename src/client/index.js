c1a4a706f7e8b0036995ebac695fae3a5a4b1392
Emiri Begic <emiri.begic@gmail.com>
2021-01-28 20:54:38 +0100

    Add project images

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 import './styles/main.scss';
 import profileImage from './img/eb-profile-img.png';
+import projectImage1 from './img/eb-travel-app.png';
+import projectImage2 from './img/eb-sentiment-analysis-app.png';
+import projectImage3 from './img/eb-weather-journal-app.png';
+import projectImage4 from './img/eb-personal-blog.png';
 import translateText from './js/translateButton';
 
 const header = document.querySelector('.page-header');
@@ -19,3 +23,15 @@ window.addEventListener('load', () => {
 // Images
 const profileImg = document.getElementById('profile-img');
 profileImg.src = profileImage;
+
+const projectImg1 = document.getElementById('project-img-1');
+projectImg1.src = projectImage1;
+
+const projectImg2 = document.getElementById('project-img-2');
+projectImg2.src = projectImage2;
+
+const projectImg3 = document.getElementById('project-img-3');
+projectImg3.src = projectImage3;
+
+const projectImg4 = document.getElementById('project-img-4');
+projectImg4.src = projectImage4;
