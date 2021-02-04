c8e30feba2961a2dcece49336ea21a4ff029ad55
Emiri Begic <emiri.begic@gmail.com>
2021-02-05 00:45:48 +0100

    Add slide-in navigation for mobile

--- src/client/index.js
+++ src/client/index.js
@@ -4,15 +4,15 @@ import projectImage1 from './img/eb-travel-app.png';
 import projectImage3 from './img/eb-weather-journal-app.png';
 import projectImage4 from './img/eb-personal-blog.png';
 
-const header = document.querySelector('.header');
-const hero = document.querySelector('.hero');
+// const header = document.querySelector('.header');
+// const hero = document.querySelector('.hero');
 
-// Dynamically get the height of the header and calculate the hight of hero
-window.addEventListener('load', () => {
-	const headerHeight = header.offsetHeight;
-	console.log(headerHeight);
-	hero.style.height = `calc(100vh - 1rem - ${headerHeight}px)`;
-});
+// // Dynamically get the height of the header and calculate the hight of hero
+// window.addEventListener('load', () => {
+// 	const headerHeight = header.offsetHeight;
+// 	console.log(headerHeight);
+// 	hero.style.height = `calc(100vh - 1rem - ${headerHeight}px)`;
+// });
 
 // Images
 const profileImg = document.querySelector('.about__img');
@@ -30,7 +30,11 @@ projectImg4.src = projectImage4;
 // Hamburger
 const hamburger = document.querySelector('.hamburger');
 const headerList = document.querySelector('.header__list');
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('is-active');
-	headerList.classList.toggle('show');
-});
+const animateNav = () => {
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('is-active');
+		headerList.classList.toggle('show');
+	});
+};
+
+animateNav();
