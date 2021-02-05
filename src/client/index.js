9edf6a59b20a3ceedad5d675cfa40021500fece2
Emiri Begic <emiri.begic@gmail.com>
2021-02-05 02:01:43 +0100

    Add animation on list items in header nav

--- src/client/index.js
+++ src/client/index.js
@@ -30,10 +30,27 @@ projectImg4.src = projectImage4;
 // Hamburger
 const hamburger = document.querySelector('.hamburger');
 const headerList = document.querySelector('.header__list');
+const headerItem = document.querySelectorAll('.header__item');
+
 const animateNav = () => {
 	hamburger.addEventListener('click', () => {
+		// Toggle classes to animate 🍔
 		hamburger.classList.toggle('is-active');
+		// Toggle classes to show / hide nav
 		headerList.classList.toggle('show');
+
+		// TODO Make it more DRY????
+		// Add animation to nav items
+		headerItem.forEach((item, i) => {
+			// console.log(i);
+			if (item.style.animation) {
+				item.style.animation = '';
+			} else {
+				item.style.animation = `navItemFade 0.7s ease forwards ${
+					i / 7 + 0.3
+				}s`;
+			}
+		});
 	});
 };
 
