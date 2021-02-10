ec1a13e08af460ce28735788afe6d87e143e4db1
Emiri Begic <emiri.begic@gmail.com>
2021-02-10 20:17:07 +0100

    Refactor: header function

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,102 +1,97 @@
+const header = document.querySelector('.header');
 const sections = document.querySelectorAll('section');
 const hamburger = document.querySelector('.hamburger');
 const headerList = document.querySelector('.header__list');
 
-// TODO there should be better way to add header as well???
-// Dynamically generate nav list items from sections (excl.hero)
-sections.forEach(section => {
-	const list = document.createElement('li');
-	const anchor = document.createElement('a');
-	list.classList.add('header__item');
-	anchor.classList.add('link');
-	anchor.setAttribute('href', `#${section.id}`);
-	anchor.innerText = section.dataset.nav;
-
-	if (anchor.innerText !== 'undefined') {
-		list.appendChild(anchor);
-		headerList.appendChild(list);
-	}
-});
-
-const headerItem = document.querySelectorAll('.header__item');
-
-// Helper - 1
-const toggleHam = () => {
-	// Toggle classes to open / close 🍔
-	hamburger.classList.toggle('is-active');
-	// Toggle classes to show / hide nav
-	headerList.classList.toggle('show');
-
-	// Prevent background scrolling when the menu is open
-	if (document.body.style.overflowY) {
-		document.body.style.overflowY = '';
-	} else {
-		document.body.style.overflowY = 'hidden';
-	}
-
-	// Add animation to nav items
-	headerItem.forEach((item, i) => {
-		// console.log(i);
-		if (item.style.animation) {
-			item.style.animation = '';
-		} else {
-			item.style.animation = `navItemFade 0.7s ease forwards ${
-				i / 7 + 0.3
-			}s`;
-		}
-	});
-};
-
-/**
- * Helper - 2
- * Add / remove click event depending on media query breakpoints
- * https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList
- */
-const screenTest = e => {
-	if (e.matches) {
-		// the viewport is 991 pixels wide or less
-		hamburger.addEventListener('click', toggleHam);
-		headerItem.forEach(item => {
-			item.addEventListener('click', toggleHam);
-		});
-	} else {
-		// the viewport is 992 pixels wide or greater
-		hamburger.removeEventListener('click', toggleHam);
-		headerItem.forEach(item =>
-			item.removeEventListener('click', toggleHam)
-		);
-	}
-};
-
-/**
- * Helper - 3
- * Smooth scroll
- */
-const scrollSmoothly = () => {
-	const links = document.querySelectorAll('.header__item a');
-
-	links.forEach(link => {
-		link.addEventListener('click', e => {
-			e.preventDefault();
-			const href = link.getAttribute('href');
-			document.querySelector(href).scrollIntoView({
-				behavior: 'smooth',
-			});
-		});
-	});
-};
-
 // Responsive Nav Menu
-const enableResponsiveNav = () => {
-	// 1. Show 🍔 menu when the viewport <= 991px
+export const enableResponsiveNav = () => {
+	// 1. Nav list items
+	const generateList = target => {
+		const list = document.createElement('li');
+		const anchor = document.createElement('a');
+		list.classList.add('header__item');
+		anchor.classList.add('link');
+		anchor.setAttribute('href', `#${target.id}`);
+		anchor.innerText = target.dataset.nav;
+
+		if (anchor.innerText !== 'undefined') {
+			list.appendChild(anchor);
+			headerList.appendChild(list);
+		}
+	};
+
+	generateList(header);
+	sections.forEach(section => generateList(section));
+	const headerItem = document.querySelectorAll('.header__item');
+
+	// 2. Toggler
+	const toggleHam = () => {
+		// Toggle classes to open / close 🍔
+		hamburger.classList.toggle('is-active');
+		// Toggle classes to show / hide nav
+		headerList.classList.toggle('show');
+
+		// Prevent background scrolling when the menu is open
+		if (document.body.style.overflowY) {
+			document.body.style.overflowY = '';
+		} else {
+			document.body.style.overflowY = 'hidden';
+		}
+
+		// Add animation to nav items
+		headerItem.forEach((item, i) => {
+			// console.log(i);
+			if (item.style.animation) {
+				item.style.animation = '';
+			} else {
+				item.style.animation = `navItemFade 0.7s ease forwards ${
+					i / 7 + 0.3
+				}s`;
+			}
+		});
+	};
+
+	/**
+	 * 3. Add / remove click event depending on media query breakpoints
+	 * https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList
+	 */
+	const screenTest = e => {
+		if (e.matches) {
+			// the viewport is 991 pixels wide or less
+			hamburger.addEventListener('click', toggleHam);
+			headerItem.forEach(item => {
+				item.addEventListener('click', toggleHam);
+			});
+		} else {
+			// the viewport is 992 pixels wide or greater
+			hamburger.removeEventListener('click', toggleHam);
+			headerItem.forEach(item =>
+				item.removeEventListener('click', toggleHam)
+			);
+		}
+	};
+
 	const smallScreen = window.matchMedia('(max-width: 991px)');
 	screenTest(smallScreen);
 	smallScreen.addEventListener('change', screenTest, false);
 	// smallScreen.onchange = () => console.log(smallScreen);
 
-	// 2. Scroll smoothly to the target
+	// 5. Smooth scroll
+	const scrollSmoothly = () => {
+		const links = document.querySelectorAll('.header__item a');
+
+		links.forEach(link => {
+			link.addEventListener('click', e => {
+				e.preventDefault();
+				const href = link.getAttribute('href');
+				document.querySelector(href).scrollIntoView({
+					behavior: 'smooth',
+				});
+			});
+		});
+	};
+
 	scrollSmoothly();
 };
-enableResponsiveNav();
 
-export { enableResponsiveNav };
+enableResponsiveNav();
