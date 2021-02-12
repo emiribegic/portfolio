d308b91b28b00216e2892ad0cf72ced4d5493275
Emiri Begic <emiri.begic@gmail.com>
2021-02-12 01:53:33 +0100

    Add dependencies to compress assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	mode: 'production',
 	entry: './src/client/index.js',
+	optimization: {
+		minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	},
 	module: {
 		rules: [
 			{
@@ -15,7 +21,7 @@ module.exports = {
 			},
 			{
 				test: /\.s[ac]ss$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 			},
 			{
 				test: /\.(png|jp(e*)g|svg)$/,
@@ -35,6 +41,9 @@ module.exports = {
 		new HtmlWebPackPlugin({
 			template: './src/client/views/index.html',
 			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
 		}),
 	],
 };
