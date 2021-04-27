7e3226633248b56fd35d34230faa528bb4fa0165
Emiri Begic <emiri.begic@gmail.com>
2021-04-27 20:22:31 +0200

    Add new project

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,15 @@ import { enableResponsiveNav } from './js/app';
 
 // Images
 import profileImage from './img/eb-profile-img.png';
+import projectImage0 from './img/eb-recipe-app.png';
 import projectImage1 from './img/eb-portfolio.png';
 import projectImage2 from './img/eb-travel-app.png';
 
 const profileImg = document.getElementById('about__img');
 profileImg.src = profileImage;
+
+const projectImg0 = document.getElementById('project-img-0');
+projectImg0.src = projectImage0;
 
 const projectImg1 = document.getElementById('project-img-1');
 projectImg1.src = projectImage1;
